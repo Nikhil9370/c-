6c17a205abb8caaaa8959fd9aca03eec2f0b0b9a
Nikhil9370 <114657266+Nikhil9370@users.noreply.github.com>
2023-04-11 21:34:45 +0530

    Add files via upload

---
+++ pointer to array.c
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+int main () {
+int *arr[3];
+int *a;
+printf("\nvalue of array pointer variable : %d"
+, arr);
+printf( "\nValue of pointer variable : %d"
+, &a);
+return 0;
+}
---
+++ structure.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <string.h>
+// create struct with person1 variable
+struct Person {
+ char name[50];
+ int age;
+ float salary;
+} person1;
+int main()
+{
+	// assign value to name of person1
+ strcpy(person1.name, "roshan patil");
+ // assign values to other person1 variables
+ person1.age= 42;
+ person1. salary = 500000;
+ // print struct variables
+ printf("Name: %s\n", person1.name);
+ printf("age: %d\n", person1.age);
+ printf("Salary: %.2f", person1.salary);
+ return 0;
+}
